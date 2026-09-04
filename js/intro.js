027980d3db58427512dfeb8d0c0ab926a6fab38e
unknown <leon.ofc.br353@gmail.com>
2026-09-03 22:19:55 -0300

    Atualiza site da Equipe África

---
+++ js/intro.js
@@ -0,0 +1,25 @@
+const intro = document.getElementById("intro");
+const site = document.getElementById("site");
+const music = document.getElementById("backgroundMusic");
+
+if (intro) {
+    intro.remove();
+}
+
+if (site) {
+    site.classList.add("is-visible");
+}
+
+if (music) {
+    music.volume = 0.35;
+
+    const startMusic = () => {
+        music.play().catch(() => {});
+    };
+
+    startMusic();
+
+    document.addEventListener("click", startMusic, { once: true });
+    document.addEventListener("touchstart", startMusic, { once: true });
+    document.addEventListener("scroll", startMusic, { once: true });
+}
